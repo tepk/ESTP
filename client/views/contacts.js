b3a73ae836e8ac03ce3048fe01b2d1dc7bff20c0
Alexey A Slyusar <alttepkuh@yandex.ru>
2015-08-30 23:25:48 +0600

    added map zoom

--- client/views/contacts.js
+++ client/views/contacts.js
@@ -6,14 +6,21 @@ Template.contacts.onRendered(function() {
         var self = this;
 
         var map;
-
+    var zoom = 10;
         DG.then(function () {
             map = DG.map('map', {
                 center: [54.903688, 83.115460],
-                zoom: 16
+                zoom: zoom
             });
             DG.marker([54.904688, 83.121270]).addTo(map).bindPopup('Единая Служба Технической Поддержки. ' + 'Проезд Садовый, д. 53, цоколь второго подъезда.');
         });
+        self.intervalId = Meteor.setInterval(function(){
+            zoom++
+            map.setZoom(zoom)
+            if(zoom === 16){
+                Meteor.clearInterval(self.intervalId);
+            }
+        }, 500);
 });
 
 Template.contacts.helpers({
